53312195d025612bb3d46185274a038de3b3e5c5
Talhah Awan <talhahawan@gmail.com>
2022-01-11 16:52:29 -0600

    Created html and generated it

--- utils/generateHtml.js
+++ utils/generateHtml.js
@@ -36,3 +36,75 @@ function generateHtml(empEngineer, empIntern, empManager) {
     </body>
     </html>`
 }
+
+function generateEngineer(empEngineer) {
+    let engineerhtml = "";
+    for (i=0; i < empEngineer.length; i ++) {
+
+        engineerhtml += `<div class="col-sm-4">
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="bg-primary text-white card-title">${empEngineer[i].name}</h2>
+                <h3 class="bg-success text-white card-title">Engineer</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="text-primary list-group-item">ID: $${empEngineer[i].id}</li>
+                    <li class="text-primary list-group-item">Email: <a href="mailto:${empEngineer[i].email}">${empEngineer[i].email}</a></li>
+                    <li class="text-danger list-group-item">GitHub: <a href="https://github.com/${empEngineer[i].gitHub.gitHub}" target="_blank" rel="noopener noreferrer">${empEngineer[i].gitHub.gitHub}</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    };
+    return engineerhtml;
+}
+
+function generateIntern(empIntern) {
+    let internhtml = "";
+    for (i=0; i < empIntern.length; i ++) {
+
+        internhtml += `<div class="col-sm-4">
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="bg-primary text-white card-title">${empIntern[i].name}</h2>
+                <h3 class="bg-success text-white card-title">Intern</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="text-primary list-group-item">ID: $${empIntern[i].id}</li>
+                    <li class="text-primary list-group-item">Email: <a href="mailto:${empIntern[i].email}">${empIntern[i].email}</a></li>
+                    <li class="text-danger list-group-item">School: ${empIntern[i].school.school}></li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    };
+    return internhtml;
+}
+
+function generateManager(empManager) {
+    let managerhtml = "";
+    for (i=0; i < empManager.length; i ++) {
+
+        managerhtml += `<div class="col-sm-4">
+        <div class="card employee-card">
+            <div class="card-header">
+                <h2 class="bg-primary text-white card-title">${empManager[i].name}</h2>
+                <h3 class="bg-success text-white card-title">Manager</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="text-primary list-group-item">ID: $${empManager[i].id}</li>
+                    <li class="text-primary list-group-item">Email: <a href="mailto:${empManager[i].email}">${empManager[i].email}</a></li>
+                    <li class="text-danger list-group-item">officeNumber: ${empManager[i].officeNumber.officeNumber}"></li>
+                </ul>
+            </div>
+        </div>
+    </div>`
+    };
+    return managerhtml;
+}
+
+module.exports = generateHtml;
+
