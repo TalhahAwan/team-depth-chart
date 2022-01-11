e6fb3798cfc189eaf23495de5c34d9ce0d07ccc8
Talhah Awan <talhahawan@gmail.com>
2022-01-11 01:09:30 -0600

    Created generate html

---
+++ utils/generateHtml.js
@@ -0,0 +1,38 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateHtml = require('./utils/generateHtml')
+
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+
+function generateHtml(empEngineer, empIntern, empManager) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css" />
+        <title>Team</title>
+    </head>
+    <body>
+        <div class = "container-fluid">
+            <div class = "row d-flex justify-content-center">
+                <h1 class = "text-center"> Workers </h1>
+            </div>
+        </div>
+
+        <div class = "container">
+            <div class = "row d-flex justify-content-center">
+                ${generateEngineer}(empEngineer)
+                ${generateIntern}(empIntern)
+                ${generateManager}(empManager)
+            </div>
+        </div>
+        
+    </body>
+    </html>`
+}
